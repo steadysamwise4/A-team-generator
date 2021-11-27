c3680f1ffce1ded1c9211b59921349cd3e732264
Sam Davenport <samueldavenport@att.net>
2021-11-26 20:12:30 -0600

    tests written for Employee.js and Manager.js, code for these classes added. Tests now pass.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,25 @@
 const Employee = require('../lib/Employee');
+const employee = new Employee('Buddy', 98452, 'buddyboy@email.com');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Buddy');
-    employee.id = 4582;
-    employee.email = 'buddyboy@email.com';
-    employee.role = 'Manager';
-
+    
     expect(employee.name).toBe('Buddy');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    expect(employee.role).toEqual(expect.any(String));
+});
+
+test('get name from getName() method', () => {
+    expect(employee.getName()).toBe('Buddy');
+});
+
+test('get id from getId() method', () => {
+    expect(employee.getId()).toBe(98452);
+});
+
+test('get email from getEmail() method', () => {
+    expect(employee.getEmail()).toBe('buddyboy@email.com');
+});
+
+test('get role from getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
 });
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,29 @@
+const Manager = require('../lib/Manager');
+const manager = new Manager('Buddy', 98452, 'buddyboy@email.com', 2 );
+
+test('creates a manager object', () => {
+    
+    expect(manager.name).toBe('Buddy');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+});
+
+test('get name from getName() method', () => {
+    expect(manager.getName()).toBe('Buddy');
+});
+
+test('get id from getId() method', () => {
+    expect(manager.getId()).toBe(98452);
+});
+
+test('get email from getEmail() method', () => {
+    expect(manager.getEmail()).toBe('buddyboy@email.com');
+});
+
+test('get role from getRole() method', () => {
+    expect(manager.getRole()).toBe('Manager');
+});
+
+test('get number from getOfficeNumber() method', () => {
+    expect(manager.getOfficeNumber()).toBe(2);
+});
